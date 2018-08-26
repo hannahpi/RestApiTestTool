c001368ad29f140fdf9fc13e84dc73aa4b46cfc6
Ben Parker <bparker@dreamersnet.net>
2018-08-25 23:10:22 -0400

    - Added response areas for the response header and status code.  This may be useful with debugging an API. - Added support for custom headers but it still needs testing, debugging, and refining to verify its implementation. - Added headers: application/json, application/x-www-form-urlencoded, application/xml, and multipart/form-data. - the form kinda looks off for now but will work on tweaking the appearance sometime next week. Resolves issues: #2 #4 Works towards implementing issue: #3

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # RestApiTestTool
 A simple tool written in JavaScript designed to (manually) test RESTful implementations.
+
+## Method Support:
+Currently this tool supports GET, POST, PATCH, UPDATE, DELETE
+
+## Request Headers:
+Currently supported: application/json, application/x-www-url-encoded, application/xml, and multipart/form-data.  Note that file uploads are not supported.  The multipart/form-data may be unnecessary since this form probably doesn't have the correct fields for you.  Then again if you're using this tool you're probably a programmer so you know what you're doing right?  You'd have to serialize, jsonify, or xmlize the object by hand though.
+
+## Custom Headers:
+I'm not sure what "custom headers" are supported right now.  You may want to reference https://www.iana.org/assignments/message-headers/message-headers.xml#prov-headers to see what headers may be supported.  I'd suspect that only those recognized as request headers will work at this time.  This needs further testing and/or research.  I'm currently using an XMLHTTP Request(XHR) so I believe it's based on what that would support.  I added a data field to facilitate X-CSRF-Token but I have no idea if it would work with XHR or not.
+
+## Action:
+This reflects the action field of a form.  This is where the request is sent.  Please note that for now you must include to full URI (include http or https).  Relative actions are not yet supported.
+
+## Request:
+This is the payload of the request.  This is used for POST, PATCH, and UPDATE requests.  Typically you'd include JSON object in your request but since you're in charge of your API this may differ.
--- api_tester.js
+++ api_tester.js
@@ -1,5 +1,20 @@
+var form;
+var selMethod;
+var txtRequest;
+var txtAction;
+var txtCHeader;
+var txtCHeaderData;
+var selCHeaders;
+var selRequestHeader;
+var txtResponseHeader;
+var btnSubmit;
+var btnAddHeader;
+var btnRemoveHeader;
+var cHeaders=new Array();
+var cDatas=new Array();
+
 //Get Data
-function sendRequest(resultObjID, method, request, url)
+function sendRequest(resultObjID, resultCodeObjID, responseHeaderID, method, request, url, headers, datas, requestHeader)
 {
 	var XMLHTTP= XMLHttpRequest || ActiveXObject("Microsoft.XMLHTTP");
 	if (typeof XMLHTTP!= "undefined" )
@@ -8,13 +23,27 @@ function sendRequest(resultObjID, method, request, url)
 		xmlhttp.onreadystatechange= function() {
 			if(xmlhttp.readyState== 4) //4 is recv'd all responses
 			{
+				var statusCode = xmlhttp.status;
+				var statusText = xmlhttp.statusText;
 				var resp = xmlhttp.responseText;
+				document.getElementById(resultCodeObjID).value = statusCode + " : " + statusText;
+				document.getElementById(responseHeaderID).innerHTML = xmlhttp.getAllResponseHeaders();
 				document.getElementById(resultObjID).innerHTML= resp;
 			}
 		}
+		xmlhttp.ontimeout= function() {
+			document.getElementById(resultCodeObjID).value = "A timeout has occurred.";
+		}
         xmlhttp.open(method, url , true);
+		for (var i=0; i<headers.length; i++) {
+			if (datas[i]=""){
+				xmlhttp.setRequestHeader("Content-type", headers[i]);
+			} else {
+				xmlhttp.setRequestHeader(headers[i], datas[i]);
+			}
+		}
         if (method != "GET") {
-		    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		    xmlhttp.setRequestHeader("Content-type", requestHeader);
 		    xmlhttp.send(request);
         } else {
             xmlhttp.send(null);
@@ -25,28 +54,58 @@ function sendRequest(resultObjID, method, request, url)
 }
 
 function sendIt() {
-    var form = document.getElementById("testForm");
-    var txtRequest = document.getElementById("request");
-    var txtAction = document.getElementById("actiontext");
     var resultObjID = "results";
-    var method = form.getAttribute("method");
+	var resultCodeObjID = "resultCode";
+	var responseHeaderID = "resultHeader";
+    var method = selMethod.value;
     var action = txtAction.value;
     var request = txtRequest.value;
-    sendRequest(resultObjID, method, request, action);
+	var requestHeader = selRequestHeader.value;
+    sendRequest(resultObjID, resultCodeObjID, responseHeaderID, method, request, action, cHeaders, cDatas, requestHeader);
+}
+
+function addHeader() {
+	if (txtCHeader.value=="") {
+		return;
+	} else {
+		var optNew = document.createElement("option");
+		optNew.text = txtCHeader.value;
+		selCHeaders.add(optNew);
+		cHeaders.push(txtCHeader.value);
+		cDatas.push(txtCHeaderData.value);
+	}
+}
+
+function removeHeader() {
+	var id = selCHeaders.selectedIndex;
+	cHeaders.splice(id, 1);
+	cDatas.splice(id, 1);
+	selCHeaders.remove(id);
 }
 
 window.onload = function() {
-    var form = document.getElementById("testForm");
-    var dropReq = document.getElementById("methodReq");
-    var txtRequest = document.getElementById("request");
-    var txtAction = document.getElementById("actiontext");
-    var btnSubmit = document.getElementById("submit");
-    dropReq.addEventListener("change", function(){
-        var form = document.getElementById("testForm");
-        form.setAttribute("method", dropReq.value);
-    }, true);
-    txtAction.addEventListener("change", function(){
-        var form = document.getElementById("testForm");
-        form.setAttribute("action", txtAction.value);
-    }, true);
+    form = document.getElementById("testForm");
+    selMethod = document.getElementById("methodReq");
+    txtRequest = document.getElementById("request");
+	selRequestHeader = document.getElementById("requestHeaderType");
+    txtAction = document.getElementById("actiontext");
+	txtCHeader = document.getElementById("CHeaderName");
+	txtCHeaderData = document.getElementById("CHeaderData");
+	selCHeaders = document.getElementById("CHeaders");
+	txtResponseHeader = document.getElementById("resultHeader");
+    btnSubmit = document.getElementById("submit");
+    btnAddHeader = document.getElementById("btnAddHeader");
+	btnRemoveHeader = document.getElementById("btnRemoveHeader");
+	document.getElementById("requestFormArea").style.display ="none";
+	selMethod.addEventListener("change", function(){
+		switch (selMethod.value) {
+			case "GET":
+			case "DELETE":
+				document.getElementById("requestFormArea").style.display="none";
+				break;
+			default:
+				document.getElementById("requestFormArea").style.display="block";
+				break;
+		}
+	});
 }
